30d66ec532b49a8a9593c01f10c935f12ac7d4c4
Mukut Brahma <mukutb.mac@gmail.com>
2025-01-17 18:01:27 +0530

    fix

--- src/components/sections/LatestNews.tsx
+++ src/components/sections/LatestNews.tsx
@@ -1,6 +1,7 @@
 import { News } from '@/types';
 import { NewsGrid } from '../news/NewsGrid';
 import SectionWrapper from '../layout/SectionWrapper';
+import Link from 'next/link';
 
 interface LatestNewsProps {
     news: News[];
@@ -11,6 +12,14 @@ export const LatestNews = ({ news }: LatestNewsProps) => {
         <SectionWrapper>
             <h2 className="text-2xl font-semibold mb-6">Latest News</h2>
             <NewsGrid news={news} />
+            <div className="flex justify-start mt-12">
+                <Link
+                    href="/news"
+                    className="text-sm px-4 py-2 border-2 border-primary hover:bg-primary hover:text-primary-foreground transition-colors"
+                >
+                    See all news
+                </Link>
+            </div>
         </SectionWrapper>
     );
 };
