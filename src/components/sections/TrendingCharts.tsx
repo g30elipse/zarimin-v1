c3490258494852d95c545aea8e9e5f75da07a1ff
Mukut Brahma <mukutb.mac@gmail.com>
2025-01-17 21:55:34 +0530

    bg

--- src/components/sections/TrendingCharts.tsx
+++ src/components/sections/TrendingCharts.tsx
@@ -8,7 +8,7 @@ interface TrendingChartsProps {
 
 export const TrendingCharts = ({ charts }: TrendingChartsProps) => {
     return (
-        <div className="bg-[#53442d] text-white md:py-32 py-16">
+        <div className="bg-card-foreground text-white md:py-32 py-16">
             <SectionWrapper>
                 <h2 className="text-2xl font-semibold mb-6 md:mb-14 text-center">Trending Charts</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
