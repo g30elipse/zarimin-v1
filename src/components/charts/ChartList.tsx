7e47609d9532227c2cf956474c4ef9313209ca2e
Mukut Brahma <mukutb.mac@gmail.com>
2025-01-18 02:31:53 +0530

    chart date

--- src/components/charts/ChartList.tsx
+++ src/components/charts/ChartList.tsx
@@ -7,10 +7,21 @@ interface ChartListProps {
 }
 
 export const ChartList = ({ chart }: ChartListProps) => {
+    // Data format is DD MMM YYYY
+    const createdAt = new Date(chart.createdAt)
+        .toLocaleDateString('en-GB', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+        })
+        .split(' ')
+        .join(' ');
+
     return (
         <Card className="rounded-card">
             <CardHeader>
                 <CardTitle>{chart.title}</CardTitle>
+                <div className="flex justify-between text-sm text-gray-500">{createdAt}</div>
             </CardHeader>
             <CardContent>
                 <div className="space-y-2">
