044315e7e772106f0c16b8f19134756b3730a7f6
Mukut Brahma <mukutb.mac@gmail.com>
2025-01-17 17:37:49 +0530

    fix

--- src/components/spotlight/ArtistSpotlight.tsx
+++ src/components/spotlight/ArtistSpotlight.tsx
@@ -9,7 +9,7 @@ export interface ArtistSpotlightProps {
 const ArtistSpotlight: FC<ArtistSpotlightProps> = (props) => {
     const { spotlights } = props;
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 md:gap-12">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 md:gap-12">
             {spotlights.map((spotlight) => (
                 <SpotlightCard key={spotlight.id} spotlight={spotlight} />
             ))}
