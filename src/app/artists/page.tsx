e822e3709f59abb3a0aaad22c0d6af74387c6d3a
Mukut Brahma <mukutb.mac@gmail.com>
2025-01-20 21:29:08 +0530

    artists page

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -5,6 +5,7 @@ import { artistsApi, spotlightApi } from '@/lib/api';
 import { OG_IMAGE_LOGO } from '@/lib/constants';
 import { ArtistsSearchParams, ServerPageProps } from '@/types';
 import { Metadata } from 'next';
+import ArtistsSearch from './_search';
 
 export const metadata: Metadata = {
     title: 'Artists - ZARIMIN',
@@ -23,12 +24,16 @@ export default async function Page(props: ServerPageProps<ArtistsSearchParams>) 
         <main className="min-h-screen py-4 md:py-8">
             <SectionWrapper>
                 <div className="flex flex-col gap-8 md:gap-16 pb-32">
-                    <div className="mb-8 md:mb-16 lg:mb-32">
-                        <ArtistSpotlight spotlights={spotlights} />
-                    </div>
                     <div className="mb-8">
-                        <h1 className="text-4xl font-bold mb-8">Artists</h1>
+                        <div className="flex md:items-center justify-between flex-col md:flex-row gap-8 mb-8">
+                            <h1 className="text-4xl font-bold">Artists</h1>
+                            <ArtistsSearch />
+                        </div>
                         <ArtistGrid artists={artists} />
+                    </div>
+                    <div className="mb-8 md:mb-16 lg:mt-32">
+                        <h1 className="text-4xl font-bold mb-8 text-center">Spotlight</h1>
+                        <ArtistSpotlight spotlights={spotlights} />
                     </div>
                 </div>
             </SectionWrapper>
