1632c4b2a722027ff78e37af0bc03e71b597bc26
Mukut Brahma <mukutb.mac@gmail.com>
2025-07-02 19:46:42 +0530

    refactor: replace smooth scrolling implementation with Hero component on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { NewsSort } from '@/types';
 import { getHomePageCharts, newsApi } from '@/lib/api';
 import { Metadata } from 'next';
 import { OG_IMAGE_LOGO } from '@/lib/constants';
-import gsap from 'gsap'
+import Hero from '@/components/sections/Hero';
 
 
 
@@ -23,36 +23,22 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-    const [news, charts, spotlightArtists, shorts] = await Promise.all([
-        getAllNews(),
-        getHomePageCharts(),
-        getArtistSpotlight(),
-        getLatestShorts(5),
-    ]);
+
+    // const [news, charts, spotlightArtists, shorts] = await Promise.all([
+    //     getAllNews(),
+    //     getHomePageCharts(),
+    //     getArtistSpotlight(),
+    //     getLatestShorts(5),
+    // ]);
 
     return (
 
-        <main className="min-h-screen  lg:space-y-44 space-y-24 pb-32 md:pb-32">
-            <div className="relative" style={{
-                backgroundImage: 'url(/hero.jpg)',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundAttachment: 'fixed',
-                height: '80vh',
-                width: '100%',
-            }}>
-                <div className="absolute inset-0 bg-black/60" />
-                <div className="absolute inset-0 px-8 flex items-center justify-center">
-                    <h1 className="lg:text-8xl text-3xl font-dancing tracking-wide text-white text-center">
-                        Preserving <span className="text-orange-400">Culture</span>
-                    </h1>
-                </div>
-            </div>
-            <LatestShorts shorts={shorts} />
+        <main className="min-h-screen h-[2000px]  lg:space-y-44 space-y-24 pb-32 md:pb-32">
+            <Hero />
+            {/* <LatestShorts shorts={shorts} />
             <LatestNews news={news} />
             <TrendingCharts charts={charts} />
-            <ArtistSpotlights spotlights={spotlightArtists} />
+            <ArtistSpotlights spotlights={spotlightArtists} /> */}
         </main>
     );
 }
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,72 @@
+'use client'
+import Lenis from 'lenis';
+import { FC, useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { useLenis } from 'lenis/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export interface HeroProps {
+
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Hero: FC<HeroProps> = (props) => {
+
+  const heroRef = useRef<HTMLDivElement>(null);
+  const textRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+
+
+    // GSAP: Animate text on scroll (opacity/y)
+    if (heroRef.current && textRef.current) {
+
+
+      // GSAP: Expand text horizontally on scroll
+      gsap.fromTo(
+        textRef.current,
+        { scaleX: 1 },
+        {
+          scaleX: 2.0,
+          // y: 400,
+          transformOrigin: "center",
+          scrollTrigger: {
+            trigger: heroRef.current,
+            start: "top top",
+            end: "bottom top",
+            scrub: true,
+          },
+        }
+      );
+    }
+
+    return () => {
+      gsap.killTweensOf(textRef.current);
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
+
+  return (
+    <div ref={heroRef} className="relative" style={{
+      backgroundImage: 'url(/hero.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed',
+      height: '80vh',
+      width: '100%',
+    }}>
+      <div className="absolute inset-0 bg-black/60" />
+      <div className="absolute inset-0 px-8 flex items-center justify-center">
+        <h1 ref={textRef} className="lg:text-8xl text-5xl font-dancing tracking-wide text-white text-center">
+          Preserving <span className="text-orange-400">Culture</span>
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+
+export default Hero;
